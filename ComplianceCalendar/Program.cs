2f9b98fd47e14b36ffe5a9ec897ca3e6632fb839
sreeram-b-kamath <107128656+sreeram-b-kamath@users.noreply.github.com>
2024-10-18 15:41:56 +0530

    Update Program.cs

--- ComplianceCalendar/Program.cs
+++ ComplianceCalendar/Program.cs
@@ -1,4 +1,4 @@
-﻿using ComplianceCalendar.Data;
+using ComplianceCalendar.Data;
 using ComplianceCalendar.Endpoints;
 using ComplianceCalendar.MappingConfig;
 using ComplianceCalendar.Repositories;
@@ -14,13 +14,21 @@ using Quartz.Impl;
 using Quartz.Spi;
 using Serilog;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 // Configure Serilog
 Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .CreateLogger();
+ &nbsp;&nbsp; .ReadFrom.Configuration(builder.Configuration)
+ &nbsp;&nbsp; .CreateLogger();
 
-builder.Host.UseSerilog(); 
+
+
+builder.Host.UseSerilog();
+
 
 
 // Add services to the container.
@@ -30,6 +38,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddTransient<IEmailSender, EmailSender>();
 
+
+
+// Register repositories
 builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
 builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
 builder.Services.AddScoped<IFilingsRepository, FilingsRepository>();
@@ -40,108 +51,153 @@ builder.Services.AddScoped<IEmailRepository, EMailRepository>();
 builder.Services.AddScoped<IUsersRepository, UsersRepository>();
 builder.Services.AddScoped<IUploadFilingsRepository, UploadFilingsRepository>();
 
+
+
+// Register services
 builder.Services.AddScoped<IPendingFilingsService, PendingFilingsService>();
 builder.Services.AddScoped<IDocNotUploadedService, DocNotUploadedService>();
 builder.Services.AddScoped<IFilingMailService, FilingMailService>();
 builder.Services.AddScoped<IFilingsService, FilingsService>();
 builder.Services.AddScoped<IDocumentService, DocumentService>();
 
-// Register your jobs
+
+
+// Register jobs
 builder.Services.AddTransient<FilingJob>();
 builder.Services.AddTransient<UpdateFilingStatusJob>();
 builder.Services.AddTransient<PendingFilingsJob>();
 builder.Services.AddTransient<DocNotUploadedJob>();
 
+
+
 // Load environment variables from .env file
 DotNetEnv.Env.Load();
+
+
 
 // Get email settings from .env file
 var emailUsername = Environment.GetEnvironmentVariable("EMAIL_USERNAME");
 var emailPassword = Environment.GetEnvironmentVariable("EMAIL_PASSWORD");
 var emailHost = Environment.GetEnvironmentVariable("EMAIL_HOST");
-var emailPort = int.Parse(Environment.GetEnvironmentVariable("EMAIL_PORT"));
+var emailPort = 587; // Default port for SMTP
+
+
 
 // Configure email settings using Options pattern
 builder.Services.Configure<EmailSettings>(options =>
 {
-    options.SenderEmail = emailUsername;
-    options.SenderPassword = emailPassword;
-    options.SmtpServer = emailHost;
-    options.SmtpPort = emailPort;
+ &nbsp;&nbsp; options.SenderEmail = emailUsername;
+ &nbsp;&nbsp; options.SenderPassword = emailPassword;
+ &nbsp;&nbsp; options.SmtpServer = emailHost;
+ &nbsp;&nbsp; options.SmtpPort = emailPort;
 });
 
-// Get connection string from .ENV file
+
+
+// Get connection string from .env file
 var connectionString = Environment.GetEnvironmentVariable("POSTGRESQL_CONNECTION_STRING");
 
+
+
+// Configure DbContexts
 builder.Services.AddDbContext<APIContext>(options =>
 {
-    options.UseNpgsql(connectionString);
+ &nbsp;&nbsp; options.UseNpgsql(connectionString);
 });
+
+
 
 builder.Services.AddDbContext<AdminContext>(options =>
 {
-    options.UseNpgsql(connectionString);
-    options.EnableSensitiveDataLogging();
-    options.LogTo(Console.WriteLine, LogLevel.Information);
+ &nbsp;&nbsp; options.UseNpgsql(connectionString);
+ &nbsp;&nbsp; options.EnableSensitiveDataLogging();
+ &nbsp;&nbsp; options.LogTo(Console.WriteLine, LogLevel.Information);
 });
+
+
 
 builder.Services.AddDbContext<ManageUserContext>(options =>
 {
-    options.UseNpgsql(connectionString);
-    options.EnableSensitiveDataLogging();
-    options.LogTo(Console.WriteLine, LogLevel.Information);
+ &nbsp;&nbsp; options.UseNpgsql(connectionString);
+ &nbsp;&nbsp; options.EnableSensitiveDataLogging();
+ &nbsp;&nbsp; options.LogTo(Console.WriteLine, LogLevel.Information);
 });
+
 
 
 // Add Quartz services
 builder.Services.AddSingleton<IJobFactory, SchedulerJobFactory>();
 builder.Services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
-/*builder.Services.AddScoped<IJob, FilingJob>();*/
+
+
 
 // Define job schedules
 builder.Services.AddSingleton(new JobSchedule(
-    jobType: typeof(FilingJob),
-    cronExpression: "0 30 16 * * ?"));
+ &nbsp;&nbsp; jobType: typeof(FilingJob),
+ &nbsp;&nbsp; cronExpression: "0 30 16 * * ?"));
+
+
 
 builder.Services.AddSingleton(new JobSchedule(
-    jobType: typeof(UpdateFilingStatusJob),
-    cronExpression: "0 00 00 * * ?"));
+ &nbsp;&nbsp; jobType: typeof(UpdateFilingStatusJob),
+ &nbsp;&nbsp; cronExpression: "0 00 00 * * ?"));
+
+
 
 builder.Services.AddSingleton(new JobSchedule(
-    jobType: typeof(PendingFilingsJob),
-    cronExpression: "0 28 17 * * ?"));
+ &nbsp;&nbsp; jobType: typeof(PendingFilingsJob),
+ &nbsp;&nbsp; cronExpression: "0 28 17 * * ?"));
+
+
 
 builder.Services.AddSingleton(new JobSchedule(
-    jobType: typeof(DocNotUploadedJob),
-    cronExpression: "0 34 16 * * ?"));
+ &nbsp;&nbsp; jobType: typeof(DocNotUploadedJob),
+ &nbsp;&nbsp; cronExpression: "0 34 16 * * ?"));
+
+
 
 builder.Services.AddHostedService<QuartzHostedService>();
 
-// Configure CORS policy for development
+
+
+// Configure CORS policy to allow requests from Vercel
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowLocalhost",
-        builder => builder
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
+ &nbsp;&nbsp; options.AddPolicy("AllowSpecificOrigin",
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; builder => builder
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; .WithOrigins("https://compliance-calendar-hosting-kkph.vercel.app") // Replace with your actual Vercel URL
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; .AllowAnyMethod()
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; .AllowAnyHeader());
 });
+
+
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseCors("AllowLocalhost");
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+
+// Apply CORS policy
+app.UseCors("AllowSpecificOrigin");
+
+
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+ &nbsp;&nbsp; app.UseSwagger();
+ &nbsp;&nbsp; app.UseSwaggerUI();
+}
+
+
+
+// Map endpoints
 app.MapDepartmentEndpoints();
 app.MapFilingEndpoints();
 app.MapAuthenticationEndpoints();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+
 
 app.Run();
